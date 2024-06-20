4a13ec3688ff7eb9b677c7377bd5f8113fadabcb
niki <nikiwone@gmail.com>
2024-06-20 18:22:22 +0300

    solved 03-accordion

--- 08-Exercise-DOM-Introduction/03. Accordion/accordion.js
+++ 08-Exercise-DOM-Introduction/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let text = document.getElementById("extra");
+    let button = document.getElementsByClassName("button")[0];
+    
+    if (button.textContent === "More") {
+        text.style.display = "block";
+        button.textContent = "Less";
+    } else if (button.textContent === "Less") {
+        text.style.display = "none";
+        button.textContent = "More";
+    }
 }
