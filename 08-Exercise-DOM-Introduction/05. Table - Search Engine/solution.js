97e573716965795c86de946f1da2f66004292060
niki <nikiwone@gmail.com>
2024-06-21 00:19:22 +0300

    solved 05. Table - Search Engine

--- 08-Exercise-DOM-Introduction/05. Table - Search Engine/solution.js
+++ 08-Exercise-DOM-Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,19 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let input = document.getElementById("searchField");
+      let container = document.getElementsByClassName("container");
+      let rows = document.querySelectorAll("table tbody tr");
+
+      for (let row of rows) {
+         if (row.textContent.includes(input.value) && input.value !== "") {
+            row.classList.add("select");
+         } else {
+            row.classList.remove("select");
+         }
+      }
+
+      input.value = "";
 
    }
 }
