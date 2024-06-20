7efbbbe450cb809521249f0f4b1857170612a1aa
niki <nikiwone@gmail.com>
2024-06-21 00:53:19 +0300

    solved 06. Format the Text

--- 08-Exercise-DOM-Introduction/06. Format the Text/solution.js
+++ 08-Exercise-DOM-Introduction/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+  let input = document.getElementById("input").value;
+  let output = document.getElementById("output");
+  let result = "";
+
+  let splittedInput = input.split(".");
+  for (let i = 0; i < splittedInput.length; i++) {
+    if (i % 3 == 0) {
+      if(splittedInput[i] === "") {
+        break;
+      }
+      result += `<p>${splittedInput[i]}.</p>`;
+    }
+  }
+
+  output.innerHTML = result;
 }
