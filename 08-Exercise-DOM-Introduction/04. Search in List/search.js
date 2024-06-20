87d2cd1f65eee1f3393bc150c52ce4f7441b6d22
niki <nikiwone@gmail.com>
2024-06-20 19:37:28 +0300

    solved 04. search in list

--- 08-Exercise-DOM-Introduction/04. Search in List/search.js
+++ 08-Exercise-DOM-Introduction/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   let input = document.getElementById("searchText");
+   let result = document.getElementById("result");
+   let towns = document.getElementById("towns");
+   let townsList = towns.querySelectorAll("ul li");
+   let matches = 0;
+
+   for (let el of townsList) {
+      if (el.textContent.includes(input.value)) {
+         el.style.fontWeight = "bold";
+         el.style.textDecoration = "underline";
+         
+         matches++;
+      } else {
+         el.style.fontWeight = "normal";
+         el.style.textDecoration = "none";
+      }
+   }
+
+   result.textContent = `${matches} matches found`;
+   input.value = "";
 }
